c25ae37bbea8634d0cf38121a9809432f61d581f
Walter Zimmerman <zimmermanw84@gmail.com>
2015-06-19 11:09:23 -0700

    UPDATE and Menu item functional MVP

--- routes/restaurants.js
+++ routes/restaurants.js
@@ -6,8 +6,6 @@ var models = require('../models');
 /* GET home page. */
 
 router.get('/restaurants', function(req, res, next) {
-  // console.log(req.user)
-
   var restaurants = models.restaurant.findAll()
     .then(function(rests) {
       res.render('restaurants', {
@@ -22,7 +20,6 @@ router.get('/restaurants', function(req, res, next) {
 router.get('/restaurants/:id', function(req, res, next) {
   var restaurant = models.restaurant.findOne( { where: { restaurant_id: req.params.id } })
     .then(function(rest) {
-    console.log("MADE IS", rest.dataValues)
       res.render('restaurant', { restaurant: rest.dataValues } );
     });
 });
@@ -30,9 +27,52 @@ router.get('/restaurants/:id', function(req, res, next) {
 router.get('/restaurants/:id/menus', function(req, res, next) {
   var restaurant = models.restaurant.findOne( { where: { restaurant_id: req.params.id }, include: [models.menu] })
     .then(function(rest) {
-
-      res.render('menus', { restaurant: rest.dataValues, menus: rest.menus } );
+        models.menu_type.findAll()
+          .then(function(types) {
+            // Add name from menu_type to menu object
+            for (var i = 0; i < types.length; i++) {
+              if (rest.menus[i].menu_type_id === types[i].dataValues.menu_type_id) {
+                rest.menus[i].name = types[i].dataValues.name;
+              }
+            }
+            res.render('menus', { restaurant: rest.dataValues, menus: rest.menus} );
+          });
     });
 });
+
+router.get('/restaurants/:restaurant_id/menus/:id', function(req, res, next) {
+  var menu = models.menu.findOne( { where: { menu_id: req.params.id }, include: [models.menu_item] })
+    .then(function(menu) {
+      res.render('menu', {
+        restaurant_id: req.params.restaurant_id,
+        menu_id: req.params.id,
+        menu_items: menu.dataValues.menu_items
+      });
+        // })
+    });
+});
+
+// EVEN WITH HIDDEN INPUT TYPE PUT, PUT ROUTE FAILS WILL CHANGE WHEN IO ADD ANGULAR
+router.post('/restaurants/:restaurant_id/menus/:menu_id/menu_items/:id', function(req, res, next) {
+  if(!req.body.description) res.send("YOU MUST ENTER description!!");
+
+  var menu_item = models.menu_item.findOne({ where: { menu_item_id: req.params.id } })
+    .then(function(item) {
+
+      item.updateAttributes({
+        description: req.body.description
+      })
+      .then(function() {
+        res.redirect('/restaurants/'+ req.params.restaurant_id +'/menus/' + req.params.menu_id);
+      })
+      .error(function(err) {
+        res.send("FAILED TO UPDATE");
+      })
+    })
+    .error(function(err) {
+      res.send("FAILED TO FIND MENU ITEM");
+    })
+});
+
 
 module.exports = router;
--- seed.js
+++ seed.js
@@ -1,6 +1,11 @@
 var yelpConfig = require("./config/auth").yelpAuth;
+var locuConfig = require("./config/auth").locuAuth;
 var models = require('./models');
+var http = require('http');
+// FAKE DATA
+var chance = require('chance').Chance();
 
+// YELP CLIENT
 var yelp = require("yelp").createClient({
   consumer_key: yelpConfig.consumerKey,
   consumer_secret: yelpConfig.consumerSecret,
@@ -8,29 +13,98 @@ var yelp = require("yelp").createClient({
   token_secret: yelpConfig.tokenSecret
 });
 
-// yelp.search({term: 'restaurants', location: 'San Francisco, Ca'}, function(error, data) {
-//   if (error) console.log(error);
+// LOCU OPTIONS: WIP GETTING BAD REQUEST ERROR The /v1_0/menu_item/search/ API endpoint is permanently disabled. See https://dev.locu.com/documentation/
 
-//   for (var i = 0; i < 10; i++) {
-//       try {
-//           models.restaurant.create({
-//             name: data.businesses[i].name,
-//             description: data.businesses[i].categories[0][0],
-//             phone_number: data.businesses[i].phone,
-//             address_1: data.businesses[i].location.address[0],
-//             city: data.businesses[i].location.city,
-//             state: data.businesses[i].location.state_code,
-//             zipcode: data.businesses[i].location.postal_code,
-//             restaurant_img: data.businesses[i].image_url,
-//             rating: data.businesses[i].rating,
-//             latitude: data.businesses[i].location.coordinate.latitude,
-//             longitude: data.businesses[i].location.coordinate.longitude
-//           }).then(function(rest) {
-//             console.log("REST: ", rest);
-//           });
-//       } catch(e) { console.log("ERROR: ", e) }
-//   }
-// });
+// var locuOptions = {
+//   host: 'https://api.locu.com',
+//   path: '/v1_0/venue/search/?name=Super%20Dupper&locality=San%20Francisco&api_key=' + locuConfig.apiKey,
+//   method: 'post'
+// };
+
+// var req = http.request(locuOptions, function(res) {
+//   // var str = '';
+
+//   console.log(res)
+//   // res.on('data', function(chunk) {
+//   //   console.log(chunk)
+//   //   str += chunk;
+//   // });
+
+//   res.on('error', function(err) {
+//     console.log(err)
+//   })
+
+//   // res.on('end', function() {
+//   //   console.log(str);
+//   // })
+// })
+
+// req.on('error', function(err) {
+//   console.log(err)
+// })
+
+// req.write("hello world!");
+// req.end();
+
+// #################################################################################
+
+
+// LOAD 10 RESTAURANTS
+
+var seedRestaurants = function() {
+  yelp.search({term: 'restaurants', location: 'San Francisco, Ca'}, function(error, data) {
+    if (error) console.log(error);
+
+    for (var i = 0; i < 10; i++) {
+        try {
+            models.restaurant.create({
+              name: data.businesses[i].name,
+              description: data.businesses[i].categories[0][0],
+              phone_number: data.businesses[i].phone,
+              address_1: data.businesses[i].location.address[0],
+              city: data.businesses[i].location.city,
+              state: data.businesses[i].location.state_code,
+              zipcode: data.businesses[i].location.postal_code,
+              restaurant_img: data.businesses[i].image_url,
+              rating: data.businesses[i].rating,
+              latitude: data.businesses[i].location.coordinate.latitude,
+              longitude: data.businesses[i].location.coordinate.longitude
+            }).then(function(rest) {
+              console.log("REST: ", rest);
+            });
+        } catch(e) { console.log("ERROR: ", e) }
+    }
+  });
+}
+
+var seedMenuItems = function() {
+  // GRAB RESTAURANTS
+  models.menu.findAll()
+    .then(function(menu) {
+
+      // EACH MENU
+      for (var i = 0; i < menu.length; i++) {
+
+      // ADD 10 MENU ITEMS
+        for (var ii = 0; ii < 10; ii++) {
+
+          models.menu_item.create({
+            menu_id: menu[i].dataValues.menu_id,
+            description: chance.sentence(),
+            name: chance.word(),
+            price: chance.natural({min: 1, max: 20}).toString(),
+            rating: chance.natural({min: 1, max: 5}).toString(),
+            item_img: 'https://placeimg.com/125/125/any'
+          })
+
+        }
+
+      }
+
+    })
+};
+
+// CREAT MENU TYPES
 
 var createMenuTypes = function() {
 
@@ -41,46 +115,45 @@ var createMenuTypes = function() {
       name: types[i]
     }).then(function(menu_type) {
       console.log(menu_type);
-    })
+    });
   }
-}
+};
+
+// CREATE MENU
 
 var createMenu = function(restaurants) {
 
-  var restIDs = [];
+  // Mock Menu types, only 3 at the moment
+  var menuTypeIDs = [1,2,3];
 
-  for(var i = 0; i < restaurants.length; i++) {
-    restIDs.push(restaurants[i].dataValues.id);
+  for(var ii = 0; ii < menuTypeIDs.length; ii++) {
+    for(var i = 0; i < restaurants.length; i++) {
+
+      models.menu.create({
+        restaurant_id: restaurants[i].dataValues.restaurant_id,
+        menu_type_id: menuTypeIDs[ii],
+        rating: (Math.floor(Math.random() * 5) + 1).toString()
+      })
+    }
   }
-
 };
 
-createMenuTypes()
 
+
+// 4 STAGE SEEDING
+
+// 1
+// seedRestaurants();
+
+// 2
+// createMenuTypes();
+
+// 3
 // var rest = models.restaurant.findAll()
-  // .then(function(rest) {
-    // createMenu(rest);
-    // console.log("REST MENU", rest);
-// })
+//   .then(function(rest) {
+//     createMenu(rest);
+// });
 
-// models.menu_type.create({ name: "B FAST" })
-  // then(function() {
-
-// })
-
-// models.menu.create({
-  // rating: "5.0",
-  // restaurant_id: 1,
-  // menu_type_id: 1
-// }).then(function(menu) {
-  // console.log(menu);
-// })
-
-// var menu = models.menu.find({ where: {menu_id: 2 }, include: [models.restaurant] })
-  // .then(function(menu) {
-    // console.log("MENU: REST", menu.restaurant)
-    // models.menu_item.find({ where: {menu_id: 2} }).then(function(menu_item) {
-      // console.log(menu_item);
-    // })
-// })
+// 4
+// seedMenuItems();
 
